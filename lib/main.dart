1703fd03b7deeaadc9b74bd392fb2597f779efbc
raffmar <rafimardi99@gmail.com>
2024-05-29 17:39:29 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number = 0;
+
+  void tekantombol() {
+    setState(() {
+      number = number + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Stateful Widget Demo",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(number.toString()),
+              MaterialButton(
+                child: Text("Tambah Bilangan"),
+                onPressed: tekantombol,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
